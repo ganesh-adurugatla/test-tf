1caf6631423c13fcd62b8b0afa7dfae8ad9e7f8c
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-14 02:47:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,9 @@
 provider "google" {
-  project = "your-project-id"
-  region  = "your-region"
+  region  = var.region
+}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.autopilot.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate)
 }
