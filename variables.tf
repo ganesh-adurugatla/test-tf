5a8ad8eae1cf619e3d72084896ad6c480269965e
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-14 02:47:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
+variable "region" {
+  description = "Region for resources"
+  type        = string
+  default     = "asia-south1"
+}
+
 variable "project_id" {
   description = "Google Cloud Project ID"
   type        = string
 }
 
-variable "region" {
-  description = "Region for resources"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "image_tag" {
-  description = "Docker image tag to deploy"
-  type        = string
+// artifact-registry.tf
+resource "google_artifact_registry_repository" "app_repository" {
+  provider      = google
+  location      = var.region
+  repository_id = "test-tf"
+  description   = "Docker repository for flask application"
+  format        = "DOCKER"
 }
