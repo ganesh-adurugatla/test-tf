8d64c7ae3e60758a93d843eda9713e0e0af547d3
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-14 14:02:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,16 +13,6 @@ terraform {
   }
 }
 
-# Artifact Registry repository
-resource "google_artifact_registry_repository" "app_repository" {
-  provider      = google
-  project       = var.project_id
-  location      = "asia-south1"
-  repository_id = "test-tf"
-  description   = "Docker repository for flask application"
-  format        = "DOCKER"
-}
-
 # Deployment configuration
 resource "google_cloud_run_service" "flask_app" {
   project  = var.project_id
@@ -41,6 +31,4 @@ resource "google_cloud_run_service" "flask_app" {
     percent         = 100
     latest_revision = true
   }
-
-  depends_on = [google_artifact_registry_repository.app_repository]
 }
