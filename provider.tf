4c12b1b0ceb1b1dd860bb0200a6811967414f368
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-14 11:42:01 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "google" {
-  region  = var.region
-}
-
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.autopilot.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate)
-}
