be0e8c757b15587da6d54c8de54a252cdead3714
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-14 11:42:34 +0530

    Delete artifact-registry.tf

--- artifact-registry.tf
+++
@@ -1,7 +0,0 @@
-resource "google_artifact_registry_repository" "app_repository" {
-  provider = google
-  location = var.region
-  repository_id = "app-repository"
-  description = "Docker repository for application images"
-  format = "DOCKER"
-}
