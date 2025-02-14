8a13acc28b23c16e75b0e532aa502511bffd1246
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-14 12:39:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,7 @@
-# terraform/main.tf
-# Configure the Google Cloud provider
 provider "google" {
   region  = "asia-south1"
 }
 
-# Configure Terraform itself
 terraform {
   required_providers {
     google = {
@@ -14,7 +11,16 @@ terraform {
   }
 }
 
-# Update existing deployment
+# This resource defines your Artifact Registry repository
+resource "google_artifact_registry_repository" "app_repository" {
+  provider      = google
+  location      = "asia-south1"
+  repository_id = "test-tf"
+  description   = "Docker repository for flask application"
+  format        = "DOCKER"
+}
+
+# This resource updates your deployment
 resource "google_cloud_run_service" "flask_app" {
   name     = "flask-app"
   location = "asia-south1"
