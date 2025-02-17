9e3be321ab0c48a3c3471d1438ed0285eda61305
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-17 12:16:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,17 @@ terraform {
   }
 }
 
+# Enable required APIs
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "artifactregistry_api" {
+  service = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
 # Deployment configuration
 resource "google_cloud_run_service" "flask_app" {
   project  = var.project_id
@@ -24,6 +35,8 @@ resource "google_cloud_run_service" "flask_app" {
       containers {
         image = "asia-south1-docker.pkg.dev/${var.project_id}/test-tf/flask-app:latest"
       }
+      # Use your custom service account
+      service_account_name = "ganesh-test@vishakha-403211.iam.gserviceaccount.com"
     }
   }
 
@@ -31,4 +44,18 @@ resource "google_cloud_run_service" "flask_app" {
     percent         = 100
     latest_revision = true
   }
+
+  depends_on = [
+    google_project_service.run_api,
+    google_project_service.artifactregistry_api
+  ]
+}
+
+# Make the service public
+resource "google_cloud_run_service_iam_member" "public_access" {
+  project  = var.project_id
+  service  = google_cloud_run_service.flask_app.name
+  location = google_cloud_run_service.flask_app.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
