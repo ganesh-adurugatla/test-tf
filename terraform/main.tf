2133b3f84a06db0259671a52930ad97d99d11f89
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-17 13:01:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,33 +12,3 @@ terraform {
     }
   }
 }
-
-# Deployment configuration
-resource "google_cloud_run_service" "flask_app" {
-  project  = var.project_id
-  name     = "flask-app"
-  location = "asia-south1"
-
-  template {
-    spec {
-      containers {
-        image = "asia-south1-docker.pkg.dev/${var.project_id}/test-tf/flask-app:latest"
-      }
-      service_account_name = "ganesh-test@vishakha-403211.iam.gserviceaccount.com"
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
-
-# Make the service public
-resource "google_cloud_run_service_iam_member" "public_access" {
-  project  = var.project_id
-  service  = google_cloud_run_service.flask_app.name
-  location = google_cloud_run_service.flask_app.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
