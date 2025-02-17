7707465f3b7c3af03b2239a343acec86aaea4a1a
ganesh-adurugatla <165656665+ganesh-adurugatla@users.noreply.github.com>
2025-02-17 13:14:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify({"message": "Hello from Cloud Run!"})
+    return jsonify({"message": "Hello from Bluecopa"})
 
 @app.route('/health')
 def health():
